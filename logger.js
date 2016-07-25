74368b261f1aed83cd80a2686183e0a21cce18e9
Lane Katris <lanekatris@gmail.com>
2016-07-24 20:33:45 -0400

    Added error handling for offline mode when can't connect to papertrail. Added some forgotten depdencies. Fixed some JS references

--- logger.js
+++ logger.js
@@ -8,14 +8,23 @@ function Logger (  ) {
             new winston.transports.Papertrail({
                 host: process.env.DASHBOARD_PAPERTRAIL_URL,
                 port: process.env.DASHBOARD_PAPERTRAIL_PORT,
-                colorize: true
+                colorize: true,
+                //handleExceptions: true,
+                //humanReadableUnhandledException: true
             }),
             new (winston.transports.Console)({
                 colorize: true,
                 level: 'verbose'
             })
+        ],
+        exceptionHandlers: [
+            new winston.transports.Console({
+                colorize: true
+            })
         ]
     });
+
+    this.logger.exitOnError = false;
 }
 
 module.exports = new Logger().logger;
